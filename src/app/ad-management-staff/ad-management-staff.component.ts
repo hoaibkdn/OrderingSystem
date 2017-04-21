4488bd8781845ab40ca05817a2e3636ad2dcc96f
Quang Ngo <ngotruongphamquang@gmail.com>
2017-04-21 15:40:49 +0700

    Add staff management function for admin

--- src/app/ad-management-staff/ad-management-staff.component.ts
+++ src/app/ad-management-staff/ad-management-staff.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin/admin.service';
+import { User } from '../models/user';
+import { WorkingTime } from '../models/working-time';
 
 @Component({
   selector: 'app-ad-management-staff',
@@ -7,9 +10,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdManagementStaffComponent implements OnInit {
 
-  constructor() { }
+	staffs: User[];
+	workingTimes: WorkingTime[];
+	staff: User;
+	workingTime: WorkingTime;
+	name: string;
+	password: string;
+	email: string;
+	confirmPassword: string;
+
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+  	this.adminService.getAllStaff().subscribe(res => {
+  		this.staffs = JSON.parse(res._body);
+  	}, err => {
+  		console.log(err);
+  	})
+  }
+
+  deleteWT(wt: WorkingTime){
+  	this.workingTime = wt;
+  }
+
+  getDetailWorkingTime(staff: User){
+  	this.staff = staff;
+  	this.adminService.getAllWorkingTime(staff.id).subscribe(res => {
+  		this.workingTimes = JSON.parse(res._body);
+  	}, err => {
+  		console.log(err);
+  	})
+  }
+  deleteWorkingTime(wt: WorkingTime){
+  	this.adminService.deleteWorkingTimeById(wt.id).subscribe(res => {
+  		if(res.status == 200){
+  			window.location.reload();
+  		}
+  	}, err => {
+  		console.log(err);
+  	})
+  }
+
+  addStaff(){
+  	console.log(this.name, this.email, this.password, this.confirmPassword);
+  	if (this.password !== this.confirmPassword){
+  		alert("Please enter match passwords");
+  	} else {
+  		let body = '{"name": "' + this.name
+	    + '", "email": "' + this.email
+	    + '", "password": "' + this.password
+	    + '", "roleId": "' + 3
+	    + '"}';
+    // console.log(JSON.parse(body));
+	    this.adminService.createStaff(JSON.parse(body)).subscribe(res => {
+	    	if (res.status == 201){
+	    		window.location.reload();
+	    	}
+	    }, err => {
+	    	console.log(err);
+	    });
+  	}
+  	
+  }
+
+  deleteStaffAccount(staff: User){
+  	this.staff = staff;
+  }
+
+  deleteStaff(staff: User){
+  	console.log(staff.name);
+  	this.adminService.deleteStaff(staff.id).subscribe(res => {
+  		if (res.status == 200){
+  			window.location.reload();
+  		}
+  	}, err => {
+  		console.log(err);
+  	})
   }
 
 }
