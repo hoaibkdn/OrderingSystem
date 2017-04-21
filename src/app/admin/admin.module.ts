cbe21e5ddb3e4219aab6af302356b83a77fcfecc
HAL <hoaibkdn2012@gmail.com>
2017-04-21 15:12:38 +0700

    push on heroku

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { NgModule }       from '@angular/core';
 import { FormsModule }    from '@angular/forms';
-// import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { CommonModule }   from '@angular/common';
 import { AdminComponent } from './admin.component';
