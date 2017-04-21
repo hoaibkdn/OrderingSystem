44ffe16984384471d98c884e73f4e09379697747
Quang Ngo <ngotruongphamquang@gmail.com>
2017-04-21 20:01:00 +0700

    Edit server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 4200);
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
