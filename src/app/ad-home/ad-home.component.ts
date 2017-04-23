c444d7a06b0e88f7981da56812dc58c4ea7fe505
Quang Ngo <ngotruongphamquang@gmail.com>
2017-04-23 20:17:19 +0700

    Add admin/home functions

--- src/app/ad-home/ad-home.component.ts
+++ src/app/ad-home/ad-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { AdminService } from '../admin/admin.service';
+import { Table } from '../models/table';
 
 @Component({
   selector: 'app-ad-home',
@@ -8,13 +10,45 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None
 })
 export class AdHomeComponent implements OnInit {
+  tables:Table[];
+  groupThreeTables: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private adminService: AdminService) { }
 
   ngOnInit() {
+    this.adminService.getAllTable().subscribe(res => {
+      this.tables = JSON.parse(res._body);
+      this.groupThreeTables = [];
+      var i = 0;
+      var temp = [];
+      for(let k = 0; k < this.tables.length; k++){
+        temp.push(this.tables[k]);
+      }
+      let numOfGroup = Math.floor(this.tables.length / 3) + 1;
+      for(let i = 0; i < numOfGroup; i++){
+        var group = [];
+        for (let l = 0; l < 3; l++){
+          console.log(l + i * 3);
+          console.log(temp[l + i * 3]);
+          group.push(temp[l + i * 3]);
+          if (l == 2){
+            this.groupThreeTables.push(group);
+          } else if((l + i * 3 + 1) == this.tables.length){
+            this.groupThreeTables.push(group);
+            break;
+          }
+        }
+      }
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  openInvoice(table: Table){
+
   }
 
   showBuild(id: number) {
-    this.router.navigate(['/detail-ordering']);
+    this.router.navigate(['/build/' + id]);
   }
 }
