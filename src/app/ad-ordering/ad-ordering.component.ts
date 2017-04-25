4a8d2be5923a0053044eb680d78666bb443c0ce4
Quang Ngo <ngotruongphamquang@gmail.com>
2017-04-25 16:58:35 +0700

    Add done for invoice in admin/home

--- src/app/ad-ordering/ad-ordering.component.ts
+++ src/app/ad-ordering/ad-ordering.component.ts
@@ -26,9 +26,12 @@ export class AdOrderingComponent implements OnInit {
     this.adminService.getAllUnpaidInvoice().subscribe(res => {
       this.unpaidInvoices = JSON.parse(res._body);
       this.route.data.subscribe((data: {tableId: number} ) => {
+        this.invoice = null;
         this.tableId = data.tableId;
         console.log('$$$ id table ', this.tableId);
+        console.log(this.unpaidInvoices.length);
         for (let i = 0; i < this.unpaidInvoices.length; i++){
+          console.log(this.unpaidInvoices[i].table.id);
           if (this.unpaidInvoices[i].table.id == this.tableId){
             this.invoice = this.unpaidInvoices[i];
             break;
@@ -46,6 +49,9 @@ export class AdOrderingComponent implements OnInit {
             }, err => {
               console.log(err);
             });
+        } else {
+          this.invoiceDetails = [];
+          this.totalMoney = 0;
         }
       });
     });
@@ -53,9 +59,19 @@ export class AdOrderingComponent implements OnInit {
 
   changeClassToBeDone(){
     let table = document.getElementsByClassName('table-order__col ' + this.tableId)[0];
-    console.log(table.classList);
-    table.classList.add('has-done');
-    let btnDone = document.getElementsByClassName('btn-done')[0];
-    btnDone.classList.add('disabled');
+    if (!table.classList.contains('has-done')){
+      this.adminService.setMadeForInvoice(this.invoice.id).subscribe(res => {
+          console.log(res._body);
+          table.classList.add('has-done');
+          table.classList.remove('is-ordering');
+          let btnDone = document.getElementsByClassName('btn-done')[0];
+          console.log(this.invoice.id, " - ", this.invoice.table.id);
+          console.log(table.classList.contains('has-done'));
+        }, err => {
+          console.log(err);
+        }
+      )
+    }
+       
   }
 }
