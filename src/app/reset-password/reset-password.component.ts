f07fc07ad33c22109eb082d7fa7ce4086809ac0c
Quang Ngo <ngotruongphamquang@gmail.com>
2017-05-11 18:39:07 +0700

    Edit reset password function

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserProfileService } from './../user-profile/user-profile.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,14 +9,39 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ResetPasswordComponent implements OnInit {
-  newPassword:string;
-  confirmNewPassword: string;
+  newPassword = "";
+  confirmNewPassword = "";
+  token = "";
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private userProfileService: UserProfileService, private router: Router) { }
 
   ngOnInit() {
     console.log('reset pass');
-
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+        let token = params['token'];
+        this.token = token;
+        console.log(token);
+      });
   }
 
+  resetPassword(){
+    if(this.token != "" && this.newPassword != ""){
+      let body = {
+        "token": this.token,
+        "newPassword": this.newPassword
+        };
+      this.userProfileService.resetPassword(body).subscribe(res => {
+        console.log(res);
+        if (res.status == 200){
+          console.log("routing");
+          localStorage.setItem('token', res.json().token);
+          console.log(localStorage.getItem('token'));
+          alert("Reset password successfully. Now you can continue using our service!");
+          this.router.navigate(['']);
+          window.location.reload();
+    
+        }
+      })
+    }
+  }
 }
