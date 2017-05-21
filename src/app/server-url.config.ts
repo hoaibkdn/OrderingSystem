2589fe189803a789c0b458f9db1030e76b2b4629
Quang Ngo <ngotruongphamquang@gmail.com>
2017-05-21 17:01:00 +0700

    Fix URL

--- src/app/server-url.config.ts
+++ src/app/server-url.config.ts
@@ -1,6 +1,5 @@
 // export const serverUrl = "https://backend-os-v2.herokuapp.com/api/";
 // export const serverLocationUrl = "https://backend-os-v2.herokuapp.com/";
-<<<<<<< HEAD
 // export const websocketUrl = "wss://backend-os-v2.herokuapp.com/";
 // export const serverUrl = "http://localhost:8080/api/";
 // export const serverUrl = "wss://backend-os-v2.herokuapp.com/";
@@ -9,10 +8,8 @@
 export const serverUrl = "http://192.168.0.103:8080/api/";
 export const serverLocationUrl = "http://192.168.0.103:8080/";
 export const websocketUrl = "ws://192.168.0.103:8080/";
-=======
-export const serverUrl = "http://localhost:8080/api/";
-export const serverLocationUrl = "http://localhost:8080/";
-// export const serverUrl = "wss://backend-os-v2.herokuapp.com/";
-export const websocketUrl = "ws://localhost:8080/";
+// export const serverUrl = "http://localhost:8080/api/";
+// export const serverLocationUrl = "http://localhost:8080/";
+// // export const serverUrl = "wss://backend-os-v2.herokuapp.com/";
+// export const websocketUrl = "ws://localhost:8080/";
 // export const websocketUrl = "wss://backend-os-v2.herokuapp.com/";
->>>>>>> master
