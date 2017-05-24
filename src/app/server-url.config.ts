389a3332110159c7e416976aa4a66315305c98a5
HAL <hoaibkdn2012@gmail.com>
2017-05-24 08:18:29 +0700

    change url

--- src/app/server-url.config.ts
+++ src/app/server-url.config.ts
@@ -14,8 +14,4 @@
 export const serverUrl = "http://localhost:8080/api/";
 export const serverLocationUrl = "http://localhost:8080/";
 export const websocketUrl = "ws://localhost:8080/";
-// export const websocketUrl = "wss://backend-os-v2.herokuapp.com/";
-// export const serverUrl = "http://192.168.0.103:8080/api/";
-// export const serverLocationUrl = "http://192.168.0.103:8080/";
-// export const websocketUrl = "ws://192.168.0.103:8080/";
 
